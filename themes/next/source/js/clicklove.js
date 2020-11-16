d13098a1ea1e71cfd639d651e49810027e618797
Yuanmin Zhou <1837722596@qq.com>
2020-11-16 18:33:00 +0800

    添加点击，出现爱心的动画

---
+++ themes/next/source/js/clicklove.js
@@ -0,0 +1,45 @@
+var a_idx = 0;
+jQuery(document).ready(function($) {
+    $("body").click(function(e) {
+        // var a = new Array("❤富强❤","❤民主❤","❤文明❤","❤和谐❤","❤自由❤","❤平等❤","❤公正❤","❤法治❤","❤爱国❤","❤敬业❤","❤诚信❤","❤友善❤");
+        var a = new Array(
+          "❤Python❤","❤Web❤","❤Docker❤","❤Linux❤",
+          "❤Git❤","❤Kali❤","❤Develop❤","❤ComputerBasics❤",
+          "❤Mysql❤","❤Redis❤","❤Mongo❤","❤Review❤",
+          "❤Messy❤","❤Management❤","❤Expansion❤","❤Xpath❤",
+          "❤Scrapy❤","❤CSS❤","❤HTML❤","❤JavaScript❤",
+          "❤Love❤","❤Ubuntu❤","❤Centos❤","❤HTTP❤",
+          "❤Flask❤","❤Django❤","❤Kafka❤","❤Selenium❤",
+          "❤HTTPS❤"
+        );
+        var $i = $("<span></span>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+        y = e.pageY;
+        $i.css({
+            "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"
+        });
+        $("body").append($i);
+        $i.animate({
+            "top": y - 180,
+            "opacity": 0
+        },
+        1500,
+        function() {
+            $i.remove();
+        });
+    });
+});
+
+
+
+
+
+
+
+
